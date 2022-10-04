02dfc9b8203728977cdbdb872b54cc8d2a23d81a
Kayna2D <kayna.silva2@etec.sp.gov.br>
2022-10-04 17:04:59 -0300

    first commit

---
+++ src/controlllers/ReportController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+module.exports = {
+    async show(req, res) {
+        const users = User.findAll({
+            attributes: ['name', 'email'],
+            where: {
+                email: {}
+            },
+        });
+    }
+};
---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const UserController = require('./controlllers/UserController');
+const AddressController = require('./controlllers/AddressController');
+const TechController = require('./controlllers/TechController');
+const ReportController = require('./controlllers/ReportController');
+
+const routes = express.Router();
+
+routes.get('/users', UserController.index);
+routes.post('/users', UserController.store);
+
+routes.get('/users/:user_id/addresses', AddressController.index);
+routes.post('/users/:user_id/addresses', AddressController.store);
+
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store);
+routes.delete('/users/:user_id/techs', TechController.delete);
+
+routes.get('/report', ReportController.show);
+
+module.exports = routes;
